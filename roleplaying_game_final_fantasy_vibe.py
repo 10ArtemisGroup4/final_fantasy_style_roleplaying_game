66640acbc2579ed8b3f65484568ffd04e7ace9df
unknown <triztancanlas0110@gmail.com>
2026-03-03 22:40:01 +0800

    hanggang dito na lang muna, imma do my code offline!

--- roleplaying_game_final_fantasy_vibe.py
+++ roleplaying_game_final_fantasy_vibe.py
@@ -1,1 +1,68 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+#game window
+bottom_panel = 150
+screen_width = 800
+screen_height = 400 + bottom_panel
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Battle")
+
+#load image
+#background image
+background_image = pygame.image.load('img/Background/background.png').convert_alpha()
+
+#panel image
+panel_image = pygame.image.load('img/Icons/panel.png').convert_alpha()
+
+#functions for drawing background
+def draw_background():
+    screen.blit(background_image, (0, 0))
+
+#function for drawing panel
+def draw_panel():
+    screen.blit(panel_image, (0, screen_height - bottom_panel))
+
+#fighter class
+class Fighter():
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        self.image = pygame.image.load(f'img/{self.name}/Idle/0.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+knight = Fighter(200, 260, "Knight", 30, 10, 3)
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    # draw background
+    draw_background()
+
+    #draw panel
+    draw_panel()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
