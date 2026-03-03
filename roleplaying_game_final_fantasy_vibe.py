921bf4222261666c16c0d110bfe235f6c06072e6
unknown <triztancanlas0110@gmail.com>
2026-03-03 23:37:54 +0800

    This is probably getting better

--- roleplaying_game_final_fantasy_vibe.py
+++ roleplaying_game_final_fantasy_vibe.py
@@ -38,13 +38,49 @@ class Fighter():
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        image = pygame.image.load(f'img/{self.name}/Idle/0.png')
-        self.image = pygame.transform.scale(image, (image.get_width() * 3, image.get_height() * 3))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.animation_list = []
+        self.frame_index = 0
+        self.action =0#0:idle, 1:attack, 2:hurt, 3:dead
+        self.update_time = pygame.time.get_ticks()
+
+        #load idle images
+        temp_list = []
+        for i in range(8):
+            image = pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            image = pygame.transform.scale(image, (image.get_width() * 3, image.get_height() * 3))
+            temp_list.append(image)
+        self.animation_list.append(temp_list)
+
+        #load attack images
+        temp_list = []
+        for i in range(8):
+            image = pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            image = pygame.transform.scale(image, (image.get_width() * 3, image.get_height() * 3))
+            temp_list.append(image)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rectification = self.image.get_rect()
+        self.rectification.center = (x, y)
+
+    def update(self):
+        animation_cooldown = 100
+
+        #handle animation
+
+        #update image
+        self.image = self.animation_list[self.action][self.frame_index]
+
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+
+        #if the animation has run out then reset  back to the start
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
 
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(self.image, self.rectification)
 
 knight = Fighter(200, 260, "Knight", 30, 10, 3)
 bandit1 = Fighter(550, 270, "Bandit", 20, 6, 1)
@@ -66,8 +102,10 @@ while run:
     draw_panel()
 
     #draw fighters
+    knight.update()
     knight.draw()
     for bandit in bandit_list:
+        bandit.update()
         bandit.draw()
 
     for event in pygame.event.get():
